1aa67153cb77f69b741266e39c1d2486ae71ca2e
Dallin Christensen <dallin@pyxiscoin.com>
2018-08-20 08:44:36 -0700

    button, card, cardMedia

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Button, CardMedia, Typography, Card } from '@material-ui/core';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Card style={{margin: '20px', padding: '20px'}}>
+          <CardMedia 
+            style={{ height: '200px' }}
+            image='https://static1.squarespace.com/static/5a6d52351f318d427dab4845/t/5b75b54288251ba6b9801b49/1534440786822/IMG_4553.jpg' />
+          <Typography>
+            Text here
+          </Typography>
+          <Typography>
+            Text here
+          </Typography>
+          <Button size='large' variant="outlined" color="secondary">
+            Hello World
+          </Button>
+        </Card>
       </div>
     );
   }
